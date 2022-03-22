ac0c040cc59b037dc67a349fa37aae20e0fc1d3a
Lucas Barp <llucas.barp@hotmail.com>
2022-03-22 20:09:49 -0300

    Primeiro Update

---
+++ src/Uri_1001.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Uri_1001{
+ 
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+     
+    int A, B, X;
+     
+    A = entrada.nextInt();
+    B = entrada.nextInt();
+    X = A + B;
+     
+    System.out.println("X = " + X);
+     
+    }
+ 
+}
